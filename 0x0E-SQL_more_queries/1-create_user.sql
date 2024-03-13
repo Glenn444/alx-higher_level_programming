7822247909687ed364e3ea3f79bb3816eabce740
Glenn444 <gmark1586@gmail.com>
2024-03-14 01:33:10 +0300

    1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Check if the user already exists
+SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1') INTO @userExists;
+
+-- Create the user only if it doesn't exist
+IF @userExists = 0 THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+END IF;
