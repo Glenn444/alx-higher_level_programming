c012c9e4f0ff67fedf6e53625cc692a1af636be3
Glenn444 <gmark1586@gmail.com>
2024-06-10 22:24:04 +0300

    Check for NULL

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,6 +12,10 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list->next;
 
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
 	while (fast != NULL && fast->next != NULL)
 	{
 		if (slow == fast)
